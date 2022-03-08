073d48682a122bbf0f5ee075543273db8cd1145c
ForeverAnApple <kinbd8@gmail.com>
2022-03-07 23:35:11 -0800

    [Cleanup] Removed unneeded imports and comments.

--- src/main.rs
+++ src/main.rs
@@ -39,23 +39,11 @@ use serenity::{
     client::bridge::gateway::GatewayIntents,
     framework::standard::{
         buckets::LimitedFor,
-        help_commands,
-        macros::{check, command, group, help, hook},
-        Args,
-        CommandGroup,
-        CommandOptions,
-        CommandResult,
-        DispatchError,
-        HelpOptions,
-        Reason,
+        macros::group,
         StandardFramework,
     },
     http::Http,
-    model::{
-        channel::Message,
-        gateway::Ready,
-        id::UserId,
-    }
+    model::gateway::Ready,
 };
 
 use sqlx::postgres::PgPoolOptions;
@@ -123,41 +111,17 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
             .with_whitespace(true)
             .on_mention(Some(bot_id))
             .prefix(">")
-            // In this case, if "," would be first, a message would never
-            // be delimited at ", ", forcing you to trim your arguments if you
-            // want to avoid whitespaces at the start of each.
             .delimiters(vec![" ", ", ", ","])
-            // Sets the bot's owners. These will be used for commands that
-            // are owners only.
             .owners(owners))
 
-        // Set a function to be called prior to each command execution. This
-        // provides the context of the command, the message that was received,
-        // and the full name of the command that will be called.
-        //
-        // Avoid using this to determine whether a specific command should be
-        // executed. Instead, prefer using the `#[check]` macro which
-        // gives you this functionality.
-        //
-        // **Note**: Async closures are unstable, you may use them in your
-        // application if you are fine using nightly Rust.
-        // If not, we need to provide the function identifiers to the
-        // hook-functions (before, after, normal, ...).
         .before(before)
-        // Similar to `before`, except will be called directly _after_
-        // command execution.
         .after(after)
-        // Set a function that's called whenever an attempted command-call's
-        // command could not be found.
         .unrecognised_command(unknown_command)
-        // Set a function that's called whenever a message is not a command.
         .normal_message(normal_message)
-        // Set a function that's called whenever a command's execution didn't complete for one
-        // reason or another. For example, when a user has exceeded a rate-limit or a command
-        // can only be performed by the bot owner.
         .on_dispatch_error(dispatch_error)
         // Can't be used more than once per 5 seconds:
-        .bucket("emoji", |b| b.delay(5)).await
+        .bucket("general", |b| b.delay(1)).await
+        .bucket("atc", |b| b.delay(5)).await
         // Can't be used more than 2 times per 30 seconds, with a 5 second delay applying per channel.
         // Optionally `await_ratelimits` will delay until the command can be executed instead of
         // cancelling the command invocation.
@@ -169,26 +133,14 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
         .await_ratelimits(1)
         // A function to call when a rate limit leads to a delay.
         .delay_action(delay_action)).await
-        // The `#[group]` macro generates `static` instances of the options set for the group.
-        // They're made in the pattern: `#name_GROUP` for the group instance and `#name_GROUP_OPTIONS`.
-        // #name is turned all uppercase
         .help(&MY_HELP)
         .group(&GENERAL_GROUP)
-        //.group(&EMOJI_GROUP)
-        //.group(&MATH_GROUP)
         .group(&ATC_GROUP)
         .group(&CB_GROUP);
-        //.group(&OWNER_GROUP);
 
     let mut client = Client::builder(&token)
         .event_handler(Handler)
         .framework(framework)
-        // For this example to run properly, the "Presence Intent" and "Server Members Intent" 
-        // options need to be enabled.
-        // These are needed so the `required_permissions` macro works on the commands that need to
-        // use it.
-        // You will need to enable these 2 options on the bot application, and possibly wait up to 5
-        // minutes.
         .intents(GatewayIntents::all())
         .type_map_insert::<CommandCounter>(HashMap::default())
         .await
