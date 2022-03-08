1f48d591f02c832ad907fb112de8c2227dc9ee8c
ForeverAnApple <kinbd8@gmail.com>
2022-03-07 23:08:59 -0800

    [Refactor] Move my help function into its own module.

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,3 +1,4 @@
 pub mod cb;
 pub mod atc;
 pub mod general;
+pub mod help;
--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ use commands::{
         debug::*,
         general::*,
     },
+    help::help::*,
 };
 
 use std::{
@@ -71,14 +72,6 @@ impl EventHandler for Handler {
 #[commands(say, latency, debug_args)]
 struct General;
 
-#[group]
-#[owners_only]
-// Limit all commands to be guild-restricted.
-#[only_in(guilds)]
-// Summary only appears when listing multiple groups.
-#[summary = "Commands for server owners"]
-struct Owner;
-
 // Rong ATC (Air Traffic Control)
 #[group]
 #[only_in(guilds)]
@@ -97,49 +90,6 @@ struct ATC;
 #[summary = "Rong Clan Battle utilities."]
 #[commands(cb_status)]
 struct CB;
-
-// The framework provides two built-in help commands for you to use.
-// But you can also make your own customized help command that forwards
-// to the behaviour of either of them.
-#[help]
-// This replaces the information that a user can pass
-// a command-name as argument to gain specific information about it.
-#[individual_command_tip = "Welcome to Rong Prime!\n\nTo learn more about a command, run help with the command's name."]
-// Some arguments require a `{}` in order to replace it with contextual information.
-// In this case our `{}` refers to a command's name.
-#[command_not_found_text = "Could not find: `{}`."]
-// Define the maximum Levenshtein-distance between a searched command-name
-// and commands. If the distance is lower than or equal the set distance,
-// it will be displayed as a suggestion.
-// Setting the distance to 0 will disable suggestions.
-#[max_levenshtein_distance(3)]
-// When you use sub-groups, Serenity will use the `indention_prefix` to indicate
-// how deeply an item is indented.
-// The default value is "-", it will be changed to "+".
-#[indention_prefix = "+"]
-// On another note, you can set up the help-menu-filter-behaviour.
-// Here are all possible settings shown on all possible options.
-// First case is if a user lacks permissions for a command, we can hide the command.
-#[lacking_permissions = "Hide"]
-// If the user is nothing but lacking a certain role, we just display it hence our variant is `Nothing`.
-#[lacking_role = "Nothing"]
-// The last `enum`-variant is `Strike`, which ~~strikes~~ a command.
-#[wrong_channel = "Strike"]
-// Serenity will automatically analyse and generate a hint/tip explaining the possible
-// cases of ~~strikethrough-commands~~, but only if
-// `strikethrough_commands_tip_in_{dm, guild}` aren't specified.
-// If you pass in a value, it will be displayed instead.
-async fn my_help(
-    context: &Context,
-    msg: &Message,
-    args: Args,
-    help_options: &'static HelpOptions,
-    groups: &[&'static CommandGroup],
-    owners: HashSet<UserId>,
-) -> CommandResult {
-    let _ = help_commands::with_embeds(context, msg, args, help_options, groups, owners).await;
-    Ok(())
-}
 
 #[hook]
 async fn before(ctx: &Context, msg: &Message, command_name: &str) -> bool {
@@ -191,27 +141,6 @@ async fn dispatch_error(ctx: &Context, msg: &Message, error: DispatchError) {
             .await;
         }
     }
-}
-
-// You can construct a hook without the use of a macro, too.
-// This requires some boilerplate though and the following additional import.
-use serenity::{futures::future::BoxFuture, FutureExt};
-fn _dispatch_error_no_macro<'fut>(
-    ctx: &'fut mut Context,
-    msg: &'fut Message,
-    error: DispatchError,
-) -> BoxFuture<'fut, ()> {
-    async move {
-        if let DispatchError::Ratelimited(info) = error {
-            if info.is_first_try {
-                let _ = msg
-                .channel_id
-                .say(&ctx.http, &format!("Try this again in {} seconds.", info.as_secs()))
-                .await;
-            }
-        };
-    }
-    .boxed()
 }
 
 #[tokio::main]
