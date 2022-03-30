af9271dacef1403bc2f9098ca45c9eaf972cf539
ForeverAnApple <kinbd8@gmail.com>
2022-03-29 22:12:05 -0700

    Update additional db migrations.
    Added CB related tables.

---
+++ migrations/20220330032832_rongbot_cb_info.sql
@@ -0,0 +1,18 @@
+-- Add migration script here
+--create types
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'channel_persona') THEN
+        CREATE TYPE channel_persona AS
+            ENUM ('cb', 'clan', 'pvp', 'public');
+    END IF;
+END$$;
+
+CREATE TABLE IF NOT EXISTS rongbot.channel_type(
+    channel_id character varying(20) NOT NULL,
+    clan_id integer REFERENCES rong_clan NOT NULL,
+    persona channel_persona NOT NULL,
+    PRIMARY KEY (channel_id, clan_id)
+);
+
+ALTER TABLE rongbot.channel_type OWNER TO rongprod;
