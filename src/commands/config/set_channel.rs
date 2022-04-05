4b5487b6612d08ca6623f28c0b3064c85172640e
ForeverAnApple <kinbd8@gmail.com>
2022-04-04 22:04:03 -0700

    Update set_channel to properly use query_as.

--- src/commands/config/set_channel.rs
+++ src/commands/config/set_channel.rs
@@ -64,13 +64,11 @@ async fn set_channel(ctx: &Context, msg: &Message, mut args: Args) -> CommandRes
     let persona = p.as_str();
     match ChannelPersona::from_str(persona) {
         Ok(persona_enum) => {
-            let ch_type: ChannelType = sqlx::query_as(
+            sqlx::query_as::<_, ChannelType>(
                   "INSERT INTO rongbot.channel_type (channel_id, clan_id, persona)
                    VALUES ($1, $2, $3)
                    ON CONFLICT (channel_id, clan_id) DO UPDATE
-                     SET persona = $3
-                   RETURNING *;",)
-
+                     SET persona = $3;",)
             .bind(mentioned_ch.to_string())
             .bind(clan_id)
             .bind(persona_enum)
@@ -80,7 +78,7 @@ async fn set_channel(ctx: &Context, msg: &Message, mut args: Args) -> CommandRes
             msg.channel_id.say(
                 ctx,
                 &format!("<#{}> has been updated to {}.",
-                         ch_type.channel_id,
+                         mentioned_ch,
                          persona)).await?;
             // println!("Channel ID: {} has been updated to {:?}.",
             //          ch_type.channel_id,
