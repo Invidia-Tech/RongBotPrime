2030fb8ef75bedccff619c6a8fb8f3a8c62a4ea6
ForeverAnApple <kinbd8@gmail.com>
2022-04-15 00:25:46 -0700

    DB Migrations for rong air traffic control.

---
+++ migrations/20220415055306_rong_atc.sql
@@ -0,0 +1,33 @@
+-- This migration adds the rong tables used for ATC (Air Traffic Control)
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'flight_status') THEN
+        CREATE TYPE flight_status AS
+            ENUM ('canceled', 'in flight', 'landed', 'crashed', 'amb');
+    END IF;
+END$$;
+
+CREATE TABLE IF NOT EXISTS rongbot.pilot(
+    pilot_id integer PRIMARY KEY NOT NULL,
+    nickname character varying(40),
+    motto text,
+    code character varying(10),
+    user_id integer REFERENCES rong_user NOT NULL
+);
+
+ALTER TABLE rongbot.pilot OWNER TO rongprod;
+
+
+CREATE TABLE IF NOT EXISTS rongbot.flight(
+    flight_id integer PRIMARY KEY NOT NULL,
+    call_sign character varying(20),
+    pilot_id integer REFERENCES rongbot.pilot NOT NULL,
+    passenger_id integer REFERENCES rong_user NOT NULL,
+    start_time timestamptz NOT NULL,
+    end_time timestamptz,
+    status flight_status NOT NULL,
+    team_id integer REFERENCES rong_team -- optional
+);
+
+ALTER TABLE rongbot.flight OWNER TO rongprod;
