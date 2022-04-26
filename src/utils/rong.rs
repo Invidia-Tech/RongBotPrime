92b53adaecf8cac1582fa91cb16793a7317ed2e0
ForeverAnApple <kinbd8@gmail.com>
2022-04-25 21:42:16 -0700

    Update ATC status.

--- src/utils/rong.rs
+++ src/utils/rong.rs
@@ -1,38 +1,38 @@
 use crate::{
-	data::DatabasePool,
-	error::RongError,
+    data::DatabasePool,
+    error::RongError,
 };
 
 use serenity::{
-	client::Context,
-	model::channel::Message,
+    client::Context,
+    model::channel::Message,
 };
 
 pub async fn get_user_id(
-	ctx: &Context,
-	msg: &Message,
-	platform_id: &String,
+    ctx: &Context,
+    msg: &Message,
+    platform_id: &String,
 ) -> Result<i32, RongError> {
-	let pool = ctx
-		.data
-		.read()
-		.await
-		.get::<DatabasePool>()
-		.cloned()
-		.unwrap();
-	match sqlx::query!(
-		"SELECT id FROM rong_user WHERE platform_id = $1;",
-		platform_id
-	)
-	.fetch_one(&pool)
-	.await
-	{
-		Ok(row) => Ok(row.id),
-		Err(_) => Err(RongError::Custom(format!(
-			"Who are you? I've never seen {} before...",
-			msg.author_nick(&ctx.http)
-				.await
-				.unwrap_or_else(|| String::from(&*msg.author.name))
-		))),
-	}
+    let pool = ctx
+        .data
+        .read()
+        .await
+        .get::<DatabasePool>()
+        .cloned()
+        .unwrap();
+    match sqlx::query!(
+        "SELECT id FROM rong_user WHERE platform_id = $1;",
+        platform_id
+    )
+    .fetch_one(&pool)
+    .await
+    {
+        Ok(row) => Ok(row.id),
+        Err(_) => Err(RongError::Custom(format!(
+            "Who are you? I've never seen {} before...",
+            msg.author_nick(&ctx.http)
+                .await
+                .unwrap_or_else(|| String::from(&*msg.author.name))
+        ))),
+    }
 }
