84be83dbe97331a1033ae5ac63ae9cc7419bf523
ForeverAnApple <kinbd8@gmail.com>
2022-05-23 21:37:10 -0700

    Migrations for new force quit table.

---
+++ migrations/20220523054931_fc.sql
@@ -0,0 +1,14 @@
+-- Force quits table
+
+DROP TABLE IF EXISTS rongbot.force_quits;
+
+CREATE TABLE IF NOT EXISTS rongbot.force_quit(
+    clanmember_id integer REFERENCES rong_clanmember NOT NULL,
+    cb_id integer REFERENCES rong_clanbattle NOT NULL,
+    time_used timestamptz NOT NULL,
+    day_used boolean NOT NULL,
+    note TEXT,
+    PRIMARY KEY (clanmember_id, cb_id, day_used)
+);
+
+ALTER TABLE rongbot.force_quit OWNER TO rongprod;
