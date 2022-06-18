aafe4a552ebfd3fa0062b87fd7714c24a7250dc1
ForeverAnApple <kinbd8@gmail.com>
2022-06-18 13:33:19 -0700

    Updated sql tables for ping.

--- migrations/20220611204140_ping.sql
+++ migrations/20220611204140_ping.sql
@@ -1,25 +1,29 @@
 -- Adds ping tables, >ping >ping
 
 CREATE TABLE IF NOT EXISTS rongbot.ping_rarity(
-    rank integer PRIMARY KEY NOT NULL,
-    weight integer NOT NULL
+    server character varying(20) NOT NULL,
+    rank integer NOT NULL,
+    weight integer NOT NULL,
+    PRIMARY KEY (server, rank)
 );
 
 ALTER TABLE rongbot.ping_rarity OWNER to rongprod;
 
-INSERT INTO rongbot.ping_rarity (rank, weight) VALUES (1, 51) ON CONFLICT DO NOTHING;
-INSERT INTO rongbot.ping_rarity (rank, weight) VALUES (2, 35) ON CONFLICT DO NOTHING;
-INSERT INTO rongbot.ping_rarity (rank, weight) VALUES (3, 10) ON CONFLICT DO NOTHING;
-INSERT INTO rongbot.ping_rarity (rank, weight) VALUES (4, 3) ON CONFLICT DO NOTHING;
-INSERT INTO rongbot.ping_rarity (rank, weight) VALUES (5, 1) ON CONFLICT DO NOTHING;
+-- INSERT INTO rongbot.ping_rarity (rank, weight) VALUES (1, 51) ON CONFLICT DO NOTHING;
+-- INSERT INTO rongbot.ping_rarity (rank, weight) VALUES (2, 35) ON CONFLICT DO NOTHING;
+-- INSERT INTO rongbot.ping_rarity (rank, weight) VALUES (3, 10) ON CONFLICT DO NOTHING;
+-- INSERT INTO rongbot.ping_rarity (rank, weight) VALUES (4, 3) ON CONFLICT DO NOTHING;
+-- INSERT INTO rongbot.ping_rarity (rank, weight) VALUES (5, 1) ON CONFLICT DO NOTHING;
 
 CREATE TABLE IF NOT EXISTS rongbot.ping_droptable(
     id SERIAL PRIMARY KEY NOT NULL,
+    server character varying(20) NOT NULL,
     user_id integer REFERENCES rong_user NOT NULL,
     rarity_rank integer NOT NULL,
     weight integer NOT NULL,
     nickname TEXT,
-    flavor_text TEXT
+    flavor_text TEXT,
+    UNIQUE (server, user_id)
 );
 
 ALTER TABLE rongbot.ping_droptable OWNER TO rongprod;
