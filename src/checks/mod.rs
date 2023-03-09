5c09747a5c9badf5b9c229690413924be9b39ebf
ForeverAnApple <kinbd8@gmail.com>
2023-03-09 12:09:38 -0800

    [Refactor] Permission utils to more easily group permissions together.

--- src/checks/mod.rs
+++ src/checks/mod.rs
@@ -1,2 +1,3 @@
 pub mod owner;
 pub mod rong_admin;
+mod util;
--- src/checks/rong_admin.rs
+++ src/checks/rong_admin.rs
@@ -1,10 +1,11 @@
-use crate::data::DatabasePool;
-
 use serenity::{
     client::Context,
     framework::standard::{macros::check, Args, CommandOptions, Reason},
     model::channel::Message,
 };
+
+use crate::checks::util::{dabo_ring, rong_admin};
+use tokio::join;
 
 #[check]
 #[name = "RongAdmin"]
@@ -14,41 +15,13 @@ async fn rong_admin_check(
     _: &mut Args,
     _: &CommandOptions,
 ) -> Result<(), Reason> {
-    // Ring/Dabo admin aboose
-    if msg.author.id == 162034086066520064 || msg.author.id == 79515100536385536 {
-        return Ok(());
-    }
-
-    let pool = ctx
-        .data
-        .read()
-        .await
-        .get::<DatabasePool>()
-        .cloned()
-        .unwrap();
-
-    let user_admin_status = match sqlx::query!(
-        "SELECT is_superadmin
-         FROM public.rong_user
-         WHERE platform_id = $1;",
-        msg.author.id.to_string()
-    )
-    .fetch_one(&pool)
-    .await
-    {
-        Ok(row) => row.is_superadmin,
-        Err(_) => {
-            return Err(Reason::User(
-                "You're definitely not a Rong superadmin! <:Suzunaaaaaaaaa:914426187378466836>"
-                    .to_string(),
-            ));
-        }
-    };
-
-    if !user_admin_status {
-        return Err(Reason::User(
-            "Nice try, but you are not a rong superadmin! <:YuiBat:964435494509346897>".to_string(),
-        ));
+    let (d, r) = join!(dabo_ring(msg), rong_admin(ctx, msg));
+    let results = vec![d, r];
+    for r in results {
+        match r {
+            Err(e) => return Err(e),
+            _ => continue,
+        };
     }
 
     Ok(())
